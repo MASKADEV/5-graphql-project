a82241dac03cf93296f7e0fa7d65412198094eb1
MASKADEV <sidkiyouness@gmail.com>
2022-07-06 16:45:19 -0700

    home componenets has been added

--- OneBlogProject/src/pages/index.tsx
+++ OneBlogProject/src/pages/index.tsx
@@ -1,16 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { PostCard, PostWidget, Categories } from "../components";
+import { getPosts } from "../../services";
 
-const posts = [
-  { title: "MASKA blog1", excerpt: "lorem text for testing our components" },
-  { title: "MASKA blog2", excerpt: "lorem text for testing our components" },
-  { title: "MASKA blog3", excerpt: "lorem text for testing our components" },
-  { title: "MASKA blog4", excerpt: "lorem text for testing our components" },
-  { title: "MASKA blog5", excerpt: "lorem text for testing our components" },
-];
-
-const Home: NextPage = () => {
+const Home: NextPage = ({ posts }: any) => {
+  console.log(posts);
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -19,15 +13,35 @@ const Home: NextPage = () => {
       <div className="grid grid-col-1 lg:grid-col-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post: any, index: number) => (
-            <PostCard title={post.title} excerpt={post.excerpt} key={index} />
+            <PostCard
+              slug={post.node.slug}
+              title={post.node.title}
+              image={post.node.featuredimage.url}
+              excerpt={post.node.excerpt}
+              key={index}
+              photo={""}
+              author={post.node.author}
+              createdAt={post.node.createdAt}
+            />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
-          <div className="lg:sticky relative top-8"></div>
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
       </div>
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
+}
 
 export default Home;
