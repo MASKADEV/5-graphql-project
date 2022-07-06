43a86b233d2c36c06ea7e1573d05d6a003cf0a45
MASKADEV <sidkiyouness@gmail.com>
2022-07-06 16:41:38 -0700

    getposts from graphqlCMS services

---
+++ OneBlogProject/services/index.ts
@@ -0,0 +1,39 @@
+import { log } from "console";
+import { request, gql } from "graphql-request";
+
+const graphqlAPI: any = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            author {
+              description
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredimage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const results = await request(graphqlAPI, query);
+  return results.postsConnection.edges;
+};
