a7ffcd37881d01baba73dbc12c57988bc04ecb96
MASKADEV <sidkiyouness@gmail.com>
2022-07-12 13:43:09 -0700

    post widget added

--- OneBlogProject/src/components/ui/PostWidget.tsx
+++ OneBlogProject/src/components/ui/PostWidget.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import Link from "next/link";
+import { getRecentPosts, getSimilarPosts } from "../../../services";
 
-const PostWidget = () => {
-  return <div>PostWidget</div>;
+interface props {
+  categories?: string;
+  slug?: string;
+}
+
+const PostWidget: React.FC<props> = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPost] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result: any) =>
+        setRelatedPost(result)
+      );
+    } else {
+      getRecentPosts().then((result: any) => setRelatedPost(result));
+    }
+  }, [slug, categories]);
+
+  console.log(relatedPosts);
+
+  return <div>Post Widget</div>;
 };
 
 export default PostWidget;
